a56a1db6f66d5f03c794e8eae45dc6d8fb4547b1
BuildTools <unconfigured@null.spigotmc.org>
2021-10-10 22:58:39 +0200

    Created GameManager class

--- src/main/java/fr/luclyoko/crystaliauhc/Main.java
+++ src/main/java/fr/luclyoko/crystaliauhc/Main.java
@@ -1,19 +1,30 @@
 package fr.luclyoko.crystaliauhc;
 
+import fr.luclyoko.crystaliauhc.game.GameManager;
 import fr.luclyoko.crystaliauhc.map.MapManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 
-    public MapManager mapManager;
+    private MapManager mapManager;
+    private GameManager gameManager;
 
     @Override
     public void onEnable() {
         mapManager = new MapManager(this);
+        gameManager = new GameManager(this);
     }
 
     @Override
     public void onDisable() {
 
+    }
+
+    public MapManager getMapManager() {
+        return mapManager;
+    }
+
+    public GameManager getGameManager() {
+        return gameManager;
     }
 }
---
+++ src/main/java/fr/luclyoko/crystaliauhc/game/GameManager.java
@@ -0,0 +1,12 @@
+package fr.luclyoko.crystaliauhc.game;
+
+import fr.luclyoko.crystaliauhc.Main;
+
+public class GameManager {
+
+    private final Main main;
+
+    public GameManager(Main main) {
+        this.main = main;
+    }
+}
